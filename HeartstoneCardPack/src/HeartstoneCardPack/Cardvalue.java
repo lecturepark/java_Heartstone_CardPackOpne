58224847b6845c48f98cff7c63dda474e16d3c4b
Kang Yeon Park <108668798+lecturepark@users.noreply.github.com>
2022-08-05 17:22:18 +0900

    Add files via upload

---
+++ HeartstoneCardPack/src/HeartstoneCardPack/Cardvalue.java
@@ -0,0 +1,11 @@
+package HeartstoneCardPack;
+
+public class Cardvalue {
+	public String cardvalue;
+
+	public Cardvalue(String cardvalue, int i) {
+		super();
+		this.cardvalue = cardvalue;
+
+	}
+}
